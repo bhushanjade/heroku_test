4b87d8fa0b3a9d82495df3362bd4b8bfaef54dac
bjade <bjade@gracenote.com>
2019-01-20 13:45:25 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
+//
+const Home = () => (
+    <div>
+        <h2>
+            Navigate to Routes using / => home & /about => about
+        </h2>
+    </div>
+);
+
+
+const NoMatch = () => (
+    <div>
+        <h2>NoMatch</h2>
+    </div>
+);
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+
+            <Switch>
+                {/*<Layout path="/users" component={Users}/>*/}
+                {/*<Layout exact path="/" component={CreateUser}/>*/}
+                <Route path="/about" component={About}/>
+                <Route exact path="/" component={Home}/>
+                {/*<Layout path="/create_user" component={CreateUsers}/>*/}
+                {/*<Route path="/create_user" component={Contact}/>*/}
+                <Route component={NoMatch} />
+            </Switch>
+
+        </Router>
+
     );
   }
 }
